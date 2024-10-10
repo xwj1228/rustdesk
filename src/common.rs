287331635ad768ebf9c6f8d8009e5630c461eb4b
xwj1228 <55219658+xwj1228@users.noreply.github.com>
2024-10-10 11:17:11 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -894,7 +894,7 @@ pub fn get_api_server(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://61.145.136.48:12280".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
